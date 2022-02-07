423c628e43464e683e57a4fa3deebb4fff0dd1ef
Laurence279 <lnunn279@gmail.com>
2022-02-07 11:32:50 +0000

    Integrate automatic grid position availability functionality for word insertion

--- pages/index.js
+++ pages/index.js
@@ -8,11 +8,10 @@ export default function Home() {
 
   const numCells = 100;
   const columnCount = 10;
-  const words = ["APPLE", "BANANA"];
-  const alphabet = "abcdefghijklmnopqrstuvwxyz"
+  const words = ["APPLE", "BANANA", "KIWI", "PINEAPPLE", "ORANGE", "MANGO", "STRAWBERRY"];
+  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
   const [letters, setLetters] = useState([]);
-  const [rows, setRows] = useState({})
 
   useEffect(() => {
 
@@ -39,40 +38,95 @@ export default function Home() {
   }, []);
 
 
-  
-  function getDirection(){
-    const directions = ["HORIZONTAL_POS", "HORIZONTAL_NEG", "VERTICAL_POS", "VERTICAL_NEG", "DIAGONAL_UP_POS", "DIAGONAL_UP_NEG", "DIAGONAL_DOWN_POS", "DIAGONAL_DOWN_NEG"];
-    const dir = directions[Math.floor(Math.random()*directions.length)];
-    return dir
-  }
 
-function findWordInsertLocation(){
+function findWordInsertLocation(wordToInsert){
 
-  const rowIndex = Math.floor(Math.random() * letters.length)
-  const row = letters[rowIndex]
-  const cellIndex = Math.floor(Math.random() * row.length)
-  return {
-    row: rowIndex,
-    cell: cellIndex
-  };
+  const insertionPositions = letters.map((row, rowIndex)=>{
+
+    return row.map((cell, cellIndex)=>{
+      return {
+        row: rowIndex,
+        cell: cellIndex,
+        directions:{
+          // Only properties that evaluate to true will be returned.
+          //spread operator is like a shorthand of Object.assign and have lower precedence than the && operator. It ignore value without property (boolean, null, undefined, number), and add all properties of the object after the ... in place. remember the && operator return the right value if true, or false otherwise. so if someCondition is true, {b : 5} will be passed to the ... operator, resulting in adding the property b to a with value 5. is someCondition is false, false will be passed to the ... operator. resulting in nothing added.
+          ...(validateWordInsertion(wordToInsert, rowIndex, cellIndex, "HORIZONTAL_POS" ) && {HORIZONTAL_POS: true}),
+          ...(validateWordInsertion(wordToInsert, rowIndex, cellIndex, "HORIZONTAL_NEG" ) && {HORIZONTAL_NEG: true}),
+          ...(validateWordInsertion(wordToInsert, rowIndex, cellIndex, "VERTICAL_POS" ) && {VERTICAL_POS:true}), 
+          ...(validateWordInsertion(wordToInsert, rowIndex, cellIndex, "VERTICAL_NEG" ) && {VERTICAL_NEG: true}), 
+          ...(validateWordInsertion(wordToInsert, rowIndex, cellIndex, "DIAGONAL_UP_POS" ) && {DIAGONAL_UP_POS: true}),
+          ...(validateWordInsertion(wordToInsert, rowIndex, cellIndex, "DIAGONAL_UP_NEG" ) && {DIAGONAL_UP_NEG: true}),
+          ...(validateWordInsertion(wordToInsert, rowIndex, cellIndex, "DIAGONAL_DOWN_POS" ) && {DIAGONAL_DOWN_POS: true}),
+          ...(validateWordInsertion(wordToInsert, rowIndex, cellIndex, "DIAGONAL_DOWN_NEG" ) && {DIAGONAL_DOWN_NEG: true})
+        }
+      }
+      
+    }).filter((cell)=>{
+      return Object.keys(cell.directions).length
+    })
+    
+
+}).filter((row)=>{
+  return row.length > 0
+})
+
+ console.log("Grid insertion locations", insertionPositions)
+
+ if(insertionPositions.length === 0) return null;
+
+const row = insertionPositions[Math.floor(Math.random() * insertionPositions.length)]
+const cell = row[Math.floor(Math.random() * row.length)]
+const directions = Object.keys(cell.directions);
+const dir = directions[Math.floor(Math.random() * directions.length)]
+return {
+  row: cell.row,
+  cell: cell.cell,
+  dir
+}
 }
 
 function handleClick(){
-  const {row, cell} = findWordInsertLocation();
-  const wordToInsert = getWord();
-  const dir = getDirection();
 
+  let wordsAdded = 0;
+  for (let i = 0; i < words.length; i ++){
+
+
+    
+  
+
+  const wordToInsert = getWord();
+
+  //Handle automatic generation of the grid. 
+    // Once a word is chosen, loop through every available cell.
+    // On each iteration, run checks in each direction to validate/invalidate word placement.
+    // Return this as an object with each directional boolean as a prop.
+    // Finally, choose a random object from this list and a (true) direction to be used as the insertion position of the word.
+    // If the returned list has no available spaces, but the word passed the pre-generation validation checks, clear all words, and restart the generation process   from the beginning.
+  
+    
+    // Return the coordinates for the word to be inserted, as well as a direction.
+    if(findWordInsertLocation(wordToInsert) === null) {
+      console.log("No spaces left!")
+      console.log("Words added:", wordsAdded)
+      return
+    }
+    const {row, cell, dir} = findWordInsertLocation(wordToInsert);
+
+    
+    console.log(row, cell, dir);
+
+    insertWord(wordToInsert, row, cell, dir)
+    wordsAdded++;
+}
+}
+
+function insertWord(wordToInsert, row, cell, dir){
   switch(dir){
     case "HORIZONTAL_POS":
         //Handle word placement if horizontal  
 
-        if(!checkWordFits(wordToInsert, row, cell, dir)) return; //Check if the word can be contained in the grid.
-        if(!validateCellPlacement(wordToInsert, row, cell, dir)) return; //Check if the word will replace any other letters in the grid.
-
-        //Place letters
         for(let i = 0; i < wordToInsert.length; i++){
         setLetters((prev)=>{
-          console.log(wordToInsert[i])
           return ([...prev.slice(0,row), [...prev[row].slice(0,cell+i), wordToInsert[i], ...prev[row].slice(cell+1+i)], ...prev.slice(row+1)])
         })
       }
@@ -81,10 +135,6 @@ function handleClick(){
     case "HORIZONTAL_NEG":
         //Handle word placement if horizontal  
 
-        if(!checkWordFits(wordToInsert, row, cell, dir)) return; //Check if the word can be contained in the grid.
-        if(!validateCellPlacement(wordToInsert, row, cell, dir)) return; //Check if the word will replace any other letters in the grid.
-
-        //Place letters
         for(let i = 0; i < wordToInsert.length; i++){
         setLetters((prev)=>{
           return ([...prev.slice(0,row), [...prev[row].slice(0,cell-i), wordToInsert[i], ...prev[row].slice(cell+1-i)], ...prev.slice(row+1)])
@@ -95,10 +145,6 @@ function handleClick(){
 
 
     case "VERTICAL_POS":
-        //Handle word placement if vertical  
-        if(!checkWordFits(wordToInsert, row, cell, dir)) return; //Check if the word can be contained in the grid.     
-        if(!validateCellPlacement(wordToInsert, row, cell, dir)) return; //Check if the word will replace any other letters in the grid.
-
         //Place letters vertically
         //Slice each row and replace one cell for every letter.
         for(let i = 0; i < wordToInsert.length; i++){
@@ -111,9 +157,6 @@ function handleClick(){
       break;
       case "VERTICAL_NEG":
         //Handle word placement if vertical  
-        if(!checkWordFits(wordToInsert, row, cell, dir)) return; //Check if the word can be contained in the grid.     
-        if(!validateCellPlacement(wordToInsert, row, cell, dir)) return; //Check if the word will replace any other letters in the grid.
-
         //Place letters vertically
         //Slice each row and replace one cell for every letter.
         for(let i = 0; i < wordToInsert.length; i++){
@@ -126,10 +169,6 @@ function handleClick(){
       break;
     case "DIAGONAL_UP_NEG":
         //Handle word placement if diagonal up left 
-
-        if(!checkWordFits(wordToInsert, row, cell, dir)) return; //Check if the word can be contained in the grid.     
-        if(!validateCellPlacement(wordToInsert, row, cell, dir)) return; //Check if the word will replace any other letters in the grid.
-
         //Slice each row and replace one cell for every letter. Decrement row, decrement cell.
         for(let i = 0; i < wordToInsert.length; i++){
           setLetters((prev)=>{
@@ -143,10 +182,6 @@ function handleClick(){
 
     case "DIAGONAL_UP_POS":
         //Handle word placement if diagonal up right
-
-          if(!checkWordFits(wordToInsert, row, cell, dir)) return; //Check if the word can be contained in the grid.     
-          if(!validateCellPlacement(wordToInsert, row, cell, dir)) return; //Check if the word will replace any other letters in the grid.
-  
           //Slice each row and replace one cell for every letter. Decrement row, increment cell.
           for(let i = 0; i < wordToInsert.length; i++){
             setLetters((prev)=>{
@@ -162,9 +197,6 @@ function handleClick(){
       break;
       case "DIAGONAL_DOWN_NEG":
         //Handle word placement if diagonal down left 
-        if(!checkWordFits(wordToInsert, row, cell, dir)) return; //Check if the word can be contained in the grid.     
-        if(!validateCellPlacement(wordToInsert, row, cell, dir)) return; //Check if the word will replace any other letters in the grid.
-
         //Place letters diagonally right
         //Slice each row and replace one cell for every letter. Increment both cell and row.
         for(let i = 0; i < wordToInsert.length; i++){
@@ -178,9 +210,6 @@ function handleClick(){
       break;
       case "DIAGONAL_DOWN_POS":
         //Handle word placement if diagonal down right 
-        if(!checkWordFits(wordToInsert, row, cell, dir)) return; //Check if the word can be contained in the grid.     
-        if(!validateCellPlacement(wordToInsert, row, cell, dir)) return; //Check if the word will replace any other letters in the grid.
-
         //Place letters diagonally right
         //Slice each row and replace one cell for every letter. Increment both cell and row.
         for(let i = 0; i < wordToInsert.length; i++){
@@ -193,8 +222,6 @@ function handleClick(){
 
       break;
   }
-  
- 
 }
 
 function getWord(){
@@ -310,15 +337,26 @@ function checkWordFits(word, row, cell, dir){
 
 }
 
-function validateCellPlacement(word, row, cell, dir){
+function validateWordInsertion(word, row, cell, dir){
+  if(!checkWordFits(word, row, cell, dir)) return false;
   const cellsToFill = getCellsToBeReplaced(word, row, cell, dir);
-  let allCellsEmpty = true;
+  let validated = true;
+  let lettersOverwritten = 0;
   cellsToFill.forEach((item)=>{
     if(!isCellEmpty(item)){
-      allCellsEmpty = false;
+
+      validated = false;
     }
+    if(isCellOverwritten(item)){
+      lettersOverwritten++;
+    }
+    if(lettersOverwritten > 1){
+      validated = false;
+    }
+    
+
   })
-  return allCellsEmpty;
+  return validated;
 }
 
 function getCellsToBeReplaced(word, row, cell, dir){
@@ -406,8 +444,6 @@ function getCellsToBeReplaced(word, row, cell, dir){
         break;
   }
 
-  console.log("Cells to be replaced..", cells, word, dir, cell, row)
-
 
   return cells
 }
@@ -417,7 +453,12 @@ function getCellsToBeReplaced(word, row, cell, dir){
 //Reusable
 
 function isCellEmpty(cell){
-  return letters[cell.row][cell.cell] === "" || letters[cell.row][cell.cell] === cell.letter ? true : false;
+
+  return letters[cell.row][cell.cell] === "" ? true : false;
+}
+
+function isCellOverwritten(cell){
+  return letters[cell.row][cell.cell] === cell.letter ? true : false;
 }
 
 
